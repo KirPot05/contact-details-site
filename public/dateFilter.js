a2b71876e95381d977472b70e10c0e0cf362b6b5
Kiran <kiran@potdar.com>
2022-10-06 13:24:12 +0530

    removed placeholder data

--- public/dateFilter.js
+++
@@ -1,9 +0,0 @@
-// const dateFilterBtn = document.getElementById("date-filter");
-// if (dateFilterBtn !== null) {
-//   dateFilterBtn.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     const startDate = document.getElementById("start_date").value;
-//     const endDate = document.getElementById("end_date").value;
-//     console.log(startDate, endDate);
-//   });
-// }
